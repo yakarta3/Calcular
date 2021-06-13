7a4303ca7bf0f6b21f716434602cdd162e93b6e3
Ernesto Ernesto <ernesto.stvz@gmail.com>
2021-06-13 22:35:29 +0100

    CalcularErnesto

---
+++ Calcular/CSFunciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular
+{
+    class CSFunciones
+    {
+        public static int Resta(int operador1, int operador2)
+        {
+            int resultado = operador1 - operador2;
+            return (resultado);
+        }
+        public static int Division(int operador1, int operador2)
+        {
+            int resultado = operador1 / operador2;
+            return (resultado);
+        }
+        public static void MostrarResultados(int operador1, int operador2, int result_resta,int result_div)
+        {
+            Console.WriteLine("Los resutados de la resta y división de " + operador1 + " y " + operador2 + " son: ");
+            Console.WriteLine("Resta: " + result_resta);
+            Console.WriteLine("División: " + result_div);
+        }
+    }
+}
--- Calcular/Program.cs
+++ Calcular/Program.cs
@@ -10,6 +10,17 @@ namespace Calcular
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Proyecto de cálculo");
+            int operador1 = 10;
+            int operador2 = 5;
+            //int suma = CSFunciones.Suma(operador1,operador2);
+            int resta = CSFunciones.Resta(operador1,operador2);
+            //int multiplicacion = CSFunciones.Multiplicacion(operador1,operador2);
+            int division = CSFunciones.Division(operador1,operador2);
+            CSFunciones.MostrarResultados(operador1,operador2,resta,division);
+
+            Console.WriteLine("Pulsa una tecla para finalizar");
+            Console.ReadKey(true);
         }
     }
 }
