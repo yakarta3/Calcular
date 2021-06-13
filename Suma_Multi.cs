51842420aacf44912f6a795cb782c349fb54ffb9
A95R03102X <samuelcasanova.int@gmail.com>
2021-06-13 22:39:52 +0100

    CalcularSamuel

---
+++ Suma_Multi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		//Suma
+		public static int Suma(int num1, int num2)
+        {
+			int suma = num1 + num2;
+			return (suma);
+        }
+
+		//Multiplicacion
+		public static int Multiplica(int num1, int num2)
+		{
+			int multiplica = num1 + num2;
+			return (multiplica);
+		}
+
+	}
+}
