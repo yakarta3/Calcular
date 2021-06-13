12dd3cab197ef518dfcfb928879f536bcab2e473
A95R03102X <samuelcasanova.int@gmail.com>
2021-06-13 23:15:18 +0100

    nice

--- Calcular/CSFunciones.cs
+++ Calcular/CSFunciones.cs
@@ -6,13 +6,39 @@ using System.Threading.Tasks;
 
 namespace Calcular
 {
-    class CSFunciones
-    {
+        class CSFunciones
         {
+            public static int Resta(int operador1, int operador2)
+            {
+                int resultado = operador1 - operador2;
+                return (resultado);
+            }
+            public static int Division(int operador1, int operador2)
+            {
+                int resultado = operador1 / operador2;
+                return (resultado);
+            }
+            public static int Suma(int num1, int num2)
+            {
+                int suma = num1 + num2;
+                return (suma);
+            }
+
+            //Multiplicacion
+            public static int Multiplicacion(int num1, int num2)
+            {
+                int multiplica = num1 + num2;
+                return (multiplica);
+            }
+            public static void MostrarResultados(int operador1, int operador2, int suma, int result_resta, int multiplicacion, int result_div)
+            {
+                Console.Write("El resultado de la suma de " + operador1 + " + " + operador2 + "es: " + suma + ".");
+                Console.Write("\nEl resultado de la multiplicacion de " + operador1 + " + " + operador2 + "es: " + multiplicacion + ".");
+                Console.WriteLine("Los resutados de la resta y división de " + operador1 + " y " + operador2 + " son: ");
+                Console.WriteLine("Resta: " + result_resta);
+                Console.WriteLine("División: " + result_div);
+
+            }
+
         }
-        {
-        }
-        {
-        }
-    }
 }
--- Calcular/Program.cs
+++ Calcular/Program.cs
@@ -10,6 +10,25 @@ namespace Calcular
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Proyecto de cálculo");
+            int operador1 = 10;
+            int operador2 = 5;
+
+            //int suma = CSFunciones.Suma(operador1,operador2);
+            int resta = CSFunciones.Resta(operador1, operador2);
+            //int multiplicacion = CSFunciones.Multiplicacion(operador1,operador2);
+            int division = CSFunciones.Division(operador1, operador2);
+
+            int suma = CSFunciones.Suma(operador1, operador2);
+            //int resta = CSFunciones.Resta(operador1,operador2);
+            int multiplicacion = CSFunciones.Multiplicacion(operador1, operador2);
+            //int division = CSFunciones.Division(operador1,operador2);
+
+            CSFunciones.MostrarResultados(operador1, operador2, suma, resta, multiplicacion, division);
+
+
+            Console.WriteLine("Pulsa una tecla para finalizar");
+            Console.ReadKey(true);
         }
     }
 }
